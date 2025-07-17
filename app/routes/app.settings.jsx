f9d02ff986b3961978784e057b42606da21412b7
Taimoor725 <taimoor725tanveer@gmail.com>
2025-07-17 15:41:21 -0700

    leaning polrize with in shopify app dev

--- app/routes/app.settings.jsx
+++ app/routes/app.settings.jsx
@@ -2,6 +2,8 @@ import { TitleBar } from '@shopify/app-bridge-react'
 import { Page } from '@shopify/polaris'
 import React from 'react'
 
+// learning how polorization work in shopify app dev with remix 
+
 function settings() {
   return (
     <Page>
